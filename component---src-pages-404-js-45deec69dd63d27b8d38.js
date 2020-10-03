99fa2be6bd53f81105ba761a018de301b26a5b34
meftunca <buraksenturk25@gmail.com>
2020-10-03 04:44:07 +0300

    Updates

--- component---src-pages-404-js-45deec69dd63d27b8d38.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("xH0s");e.default=function(){return Object(i.f)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-45deec69dd63d27b8d38.js.map
